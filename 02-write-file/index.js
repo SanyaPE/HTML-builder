a516285ab633b242185ed610d8c752a8da771b75
Alexander Pepler <pepleralex@gmail.com>
2024-01-22 23:27:08 +0300

    fix: chenge sync function to async

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,23 +1,25 @@
 const { stdin, stdout } = process;
-const fs = require('fs');
+const { appendFile } = require('node:fs/promises');
 const path = require('path');
 const filePath = path.join(__dirname, 'text.txt');
 
-stdout.write("enter text or press 'ctrl+C' to exit and save\n");
-stdin.on('data', (data) => {
-  let str = data.toString().slice(0, -2);
-  if (str === 'exit') {
-    console.log('"File saved. Bye!');
-    process.exit();
-  }
-  fs.appendFile(filePath, `${str}\n`, (err) => {
-    if (err) {
-      throw err;
-    }
-    console.log('text added');
-    process.on('SIGINT', () => {
-      console.log('"File saved. Bye!');
-      process.exit();
+(async () => {
+  try {
+    stdout.write("enter text or press 'ctrl+C' to exit and save\n");
+    stdin.on('data', (data) => {
+      let str = data.toString().slice(0, -2);
+      if (str === 'exit') {
+        console.log('File saved. Bye!');
+        process.exit();
+      }
+      appendFile(filePath, `${str}\n`);
+      console.log('text added');
+      process.on('SIGINT', () => {
+        console.log('File saved. Bye!');
+        process.exit();
+      });
     });
-  });
-});
+  } catch (error) {
+    console.error(err.message);
+  }
+})();
